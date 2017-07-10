6817c24d3afb303d0f30b2850d60aed08a6965a7
BbChip <qazwsx10000@naver.com>
2017-07-10 20:06:30 +0900

    Update client and server

--- asynch_serer_test/serer_test/asynck_client_test.cpp
+++ asynch_serer_test/serer_test/asynck_client_test.cpp
@@ -19,51 +19,53 @@ enum myEnum
 boost::mutex io_mutex;
 
 void receive_handle(tcp::socket * _p_socket) {
-	// 읽어올 데이터를 저장하기 위해 배열 선언
-	boost::array<char, eBuffSize> buf;
-	boost::system::error_code error;
+	while (true) {
+		// 읽어올 데이터를 저장하기 위해 배열 선언
+		boost::array<char, eBuffSize> buf;
+		boost::system::error_code error;
 
-	// read_some 함수를 이용해 서버로부터 메시지를 전달 받음
-	size_t len = _p_socket->read_some(boost::asio::buffer(buf), error);
+		// read_some 함수를 이용해 서버로부터 메시지를 전달 받음
+		size_t len = _p_socket->read_some(boost::asio::buffer(buf), error);
 
-	// 해당 서버는 전송 이후에 접속이 끊어지므로 error:eof 리턴
-	if (error == boost::asio::error::eof) {
-		//break;
-		exit(0);
+		// 해당 서버는 전송 이후에 접속이 끊어지므로 error:eof 리턴
+		if (error == boost::asio::error::eof) {
+			//break;
+			exit(0);
+		}
+		else if (error)
+			throw boost::system::system_error(error);
+
+		// 입력 받은 데이터를 cout를 이용해 출력
+		std::cout << "받은 메세지 : ";
+		std::cout.write(buf.data(), len);
+		std::cout << std::endl;
 	}
-	else if (error)
-		throw boost::system::system_error(error);
-
-	//boost::mutex::scoped_lock lock(io_mutex);
-
-	// 입력 받은 데이터를 cout를 이용해 출력
-	std::cout << "받은 메세지 : ";
-	std::cout.write(buf.data(), len);
-	std::cout << std::endl;
 }
 
 void send_handle(tcp::socket * _p_socket) {
-	// 보낼 데이터를 저장하기 위해 배열 선언
-	boost::array<char, eBuffSize> buf_send;
-	boost::system::error_code error;
+	while (true) {
+		// 보낼 데이터를 저장하기 위해 배열 선언
+		boost::array<char, eBuffSize> buf_send;
+		boost::system::error_code error;
 
-	// 클라이언트에서 서버로 보낼 메세지를 입력받음
-	std::cout << "보낼 메세지 : ";
-	//boost::mutex::scoped_lock lock(io_mutex);
-	std::cin.getline(buf_send.data(), eBuffSize);
-	int len_str = std::cin.gcount() - 1;
+		// 클라이언트에서 서버로 보낼 메세지를 입력받음
+		std::cout << "보낼 메세지 : ";
+		std::cin.getline(buf_send.data(), eBuffSize);
+		int len_str = std::cin.gcount() - 1;
 
-	// 입력받은 문자열을 송신
-	int len_send = boost::asio::write(*_p_socket
-		, boost::asio::buffer(buf_send, len_str)
-		, error);
-	if (len_send > 0) {
-		std::cout << "보내진 메세지 : ";
-		std::cout.write(buf_send.data(), len_str);
-		std::cout << std::endl;
-	}
-	else {
-		std::cout << "error!" << std::endl;
+		// 입력받은 문자열을 송신
+		int len_send = boost::asio::write(*_p_socket
+										, boost::asio::buffer(buf_send, len_str)
+										, error);
+		if (len_send > 0) {
+			std::cout << "보내진 메세지 : ";
+			std::cout.write(buf_send.data(), len_str);
+			std::cout << std::endl;
+		}
+		else {
+			std::cout << "error!" << std::endl;
+		}
+
 	}
 }
 
@@ -97,15 +99,17 @@ int _tmain(int argc, char* argv[])
 			throw boost::system::system_error(error);
 
 		// 접속에 성공한 경우
-		std::cout << "서버와 클라이언트 동기식 1대1 채팅 시작" << std::endl << std::endl;
-		while (true)
-		{
-			boost::thread thrd_rec(boost::bind(&receive_handle, &socket));
-			boost::thread thrd_snd(boost::bind(&send_handle, &socket));
-
-			thrd_rec.join();
-			thrd_snd.join();
-		}
+		std::cout << "서버와 클라이언트 비동기식 1대1 채팅 시작" << std::endl << std::endl;
+		
+		boost::thread thrd_rec(boost::bind(&receive_handle, &socket));
+		boost::thread thrd_snd(boost::bind(&send_handle, &socket));
+		
+		thrd_rec.join();
+		thrd_snd.join();
+		//while (true)
+		//{
+		//	
+		//}
 	}
 	catch (std::exception& e)
 	{
